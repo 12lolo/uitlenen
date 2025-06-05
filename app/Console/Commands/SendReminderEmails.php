37c94b3404e0d5948edb42060906d38d6a5491b1
12lolo <sennevisser@outlook.com>
2025-06-05 10:04:47 +0200

    set up task schedulers and updated mailer for automisation

--- app/Console/Commands/SendReminderEmails.php
+++ app/Console/Commands/SendReminderEmails.php
@@ -12,9 +12,7 @@ use App\Mail\OverdueReminder;
 class SendReminderEmails extends Command
 {
     protected $signature = 'email:reminders';
-    protected $description = 'Send reminder emails for items due soon or overdue';
-
-    public function handle()
+    protected $description = 'Send reminder emails for items due soon or overdue';    public function handle()
     {
         $this->sendDueReminders();
         $this->sendOverdueReminders();
@@ -28,36 +26,32 @@ class SendReminderEmails extends Command
         // Find loans that are due in 24 hours
         $tomorrow = Carbon::tomorrow();
         $dueLoans = Loan::whereNull('returned_at')
-            ->whereDate('return_date', $tomorrow)
-            ->get();
-
-        $this->info("Found {$dueLoans->count()} loans due tomorrow");
+            ->whereDate('due_date', $tomorrow)
+            ->get();        $this->info("Found {$dueLoans->count()} loans due tomorrow");
 
         foreach ($dueLoans as $loan) {
-            // Only send if we have borrower email (could be in Loan model or related to it)
-            if ($loan->borrower_email) {
-                Mail::to($loan->borrower_email)
+            // Only send if we have student email
+            if ($loan->student_email) {
+                Mail::to($loan->student_email)
                     ->send(new DueReminder($loan));
 
                 $this->line("Sent due reminder for loan #{$loan->id}");
             }
         }
-    }
-
-    private function sendOverdueReminders()
+    }    private function sendOverdueReminders()
     {
         // Find loans that were due yesterday and still not returned
         $yesterday = Carbon::yesterday();
         $overdueLoans = Loan::whereNull('returned_at')
-            ->whereDate('return_date', $yesterday)
+            ->whereDate('due_date', $yesterday)
             ->get();
 
         $this->info("Found {$overdueLoans->count()} loans that became overdue yesterday");
 
         foreach ($overdueLoans as $loan) {
-            // Only send if we have borrower email
-            if ($loan->borrower_email) {
-                Mail::to($loan->borrower_email)
+            // Only send if we have student email
+            if ($loan->student_email) {
+                Mail::to($loan->student_email)
                     ->send(new OverdueReminder($loan));
 
                 $this->line("Sent overdue reminder for loan #{$loan->id}");
--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HelpController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\FormatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,7 +26,9 @@ Route::get('/help/authenticated', [HelpController::class, 'authenticatedHelp'])-
 
 // Authentication routes
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/login/format', [FormatController::class, 'loginFormat']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::get('/register/format', [FormatController::class, 'registerFormat']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // User profile route
@@ -40,39 +43,50 @@ Route::get('/items/{id}', [ItemController::class, 'show']);
 
 // Test routes
 Route::post('/test/email', [TestController::class, 'testEmail']);
+Route::get('/test/email/format', [FormatController::class, 'testEmailFormat']);
 
 // Protected routes - authentication required
 Route::middleware('auth:sanctum')->group(function () {
     // Category management
     Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/format', [FormatController::class, 'categoryFormat']);
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::get('/categories/update/format', [FormatController::class, 'categoryUpdateFormat']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
     // Item management
     Route::get('/items', [ItemController::class, 'index']);
     Route::post('/items', [ItemController::class, 'store']);
+    Route::get('/items/format', [FormatController::class, 'itemFormat']);
     Route::put('/items/{id}', [ItemController::class, 'update']);
+    Route::get('/items/update/format', [FormatController::class, 'itemUpdateFormat']);
     Route::delete('/items/{id}', [ItemController::class, 'destroy']);
 
     // Loan management
     Route::get('/lendings', [LoanController::class, 'index']);
     Route::post('/lendings', [LoanController::class, 'store']);
+    Route::get('/lendings/format', [FormatController::class, 'lendingFormat']);
     Route::post('/lendings/{id}/return', [LoanController::class, 'returnItem']);
+    Route::get('/lendings/return/format', [FormatController::class, 'returnItemFormat']);
     Route::get('/lendings/status', [LoanController::class, 'status']);
 
     // Damage management
     Route::post('/items/{id}/damage', [DamageController::class, 'store']);
+    Route::get('/items/damage/format', [FormatController::class, 'damageFormat']);
     Route::get('/damages', [DamageController::class, 'index']);
 
     // User management (admin only)
     Route::middleware('admin')->group(function () {
         Route::get('/users', [UserController::class, 'index']);
         Route::post('/users', [UserController::class, 'store']);
+        Route::get('/users/format', [FormatController::class, 'userFormat']);
     });
 });
 
 // Notification route (protected by token for cron jobs)
 Route::post('/notifications/send-reminders', [NotificationController::class, 'sendReminders']);
+Route::get('/notifications/send-reminders/format', [FormatController::class, 'sendRemindersFormat']);
 
 // Legacy project routes
 Route::apiResource('projects', ProjectController::class);
+Route::get('/projects/format', [FormatController::class, 'projectFormat']);
